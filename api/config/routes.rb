e8e88efce8f10b8db1ea68e8fbfa42cad6f1a1fd
Hideyuki Matsuyama <choripon@gmail.com>
2025-06-02 00:33:45 +0900

    bin/rubocop -A

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  scope '/api' do
+  scope "/api" do
     resources :examples
   end
 
